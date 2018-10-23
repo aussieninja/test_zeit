5c231b2fd11799475a4007a7956f5112e9d6f331
aussieninja <44412660+aussieninja@users.noreply.github.com>
2018-10-23 14:11:40 -0500

    Update app.js
    See if I can get anything to the screen

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const name = "Andrew";
+const details = { favouriteLanguage: "JavaScript", age: 33, children: 3 } 
+const errorMessage = "Something bad has occurred";
+console.log( name );
+console.dir( details );
+console.error( errorMessage );
